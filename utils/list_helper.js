1d84c9593ddcce16df80bc585bf9ce35adabea0f
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2022-08-22 13:45:38 -0700

    feat: exercise 4.4 - adds totalLikes function and its tests (zero, one, multiple)

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,7 @@
-const dummy = (blogs) => {
-  return 1
+const totalLikes = (blogs) => {
+  return blogs.map(blog => blog.likes).reduce((sum, current) => sum + current, 0)
 }
 
 module.exports = {
-  dummy
+  totalLikes
 }
