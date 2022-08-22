c2b9a9f806ec08d8cd1c144b04a847f98fe96124
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2022-08-21 17:06:28 -0700

    refactor: exercise 4.2 - pulls out env var handling into separate module, pulls out port number into env

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
+const config = require('./utils/config')
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -15,7 +16,7 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
     .then(result => {
         logger.info('connected to MongoDB')
@@ -45,7 +46,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
