0e7e661ee21ed70758b12a803f5279ac880411d3
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2022-08-21 17:39:46 -0700

    refactor: exercise 4.2 - pulls out blog model as own module

--- index.js
+++ index.js
@@ -6,15 +6,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
