5653126dc2285a7e41ed160d5b0472d843f67675
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2022-11-11 23:01:04 -0800

    exercise 4.8 - refactor: cleans up index.js in preparation for new test suite

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-require('dotenv').config()
+const app = require('./app') // varsinainen Express-sovellus
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
-const logger = require('./utils/logger')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
-    .then(result => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connecting to MongoDB:', error.message)
-    })
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
