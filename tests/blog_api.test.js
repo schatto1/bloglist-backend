87af7dcfbc3c2bf89f9c04708168964b6a4342d7
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2022-11-11 23:55:43 -0800

    chore: installs express-async-errors and cross-env

---
+++ tests/blog_api.test.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
+
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
