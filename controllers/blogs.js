ce66be1138337599062f847845ebfbe90221571d
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-02-02 18:49:42 -0800

    exercise 4.13 - refactor: update put functionality to use async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -25,7 +25,7 @@ blogsRouter.delete('/:id', async (request, response) => {
   response.status(204).end()
 })
 
-blogsRouter.put('/:id', (request, response, next) => {
+blogsRouter.put('/:id', async (request, response) => {
   const body = request.body
 
   const blog = {
@@ -33,11 +33,8 @@ blogsRouter.put('/:id', (request, response, next) => {
     likes: body.likes,
   }
 
-  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-    .then(updatedBlog => {
-      response.json(updatedBlog)
-    })
-    .catch(error => next(error))
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
