2de105fec34c59b2cebc3a34e00237ccf998c409
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-02-02 18:22:15 -0800

    exercise 4.13 - refactor: now uses describe to group tests

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -13,100 +13,106 @@ beforeEach(async () => {
   await Blog.insertMany(helper.initialBlogs)
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('all blog posts are returned', async () => {
+    const response = await api.get('/api/blogs')
+  
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('blog posts have unique identifier property "id"', async () => {
+    const response = await api.get('/api/blogs')
+  
+    response.body.forEach(blog => expect(blog.id).toBeDefined())
+  })
 })
 
-test('all blog posts are returned', async () => {
-  const response = await api.get('/api/blogs')
-
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
+describe('addition of a new blog', () => {
+  test('POST request creates a new blog post', async () => {
+    const newBlog = {
+      title: 'this is a newly added blog post',
+      author: "Testy McTesterson",
+      url: "added-by-test",
+      likes: 6
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  
+    const contents = blogsAtEnd.map(n => n.title)
+    expect(contents).toContain(
+      'this is a newly added blog post'
+    )
+  })
+  
+  test('if likes missing, defaults to zero', async () => {
+    const newBlog = {
+      title: 'this blog post is missing likes',
+      author: "Likey McLikerson",
+      url: "added-by-test-again"
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  
+    const blogToView = blogsAtEnd[blogsAtEnd.length - 1]
+  
+    expect(blogToView.likes).toEqual(0)
+  })
+  
+  test('blogs without title is not added', async () => {
+    const newBlog = {
+      author: "Missing McMisserson",
+      url: "added-by-test-three",
+      likes: 21
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
+  
+  test('blogs without url is not added', async () => {
+    const newBlog = {
+      title: "URL is missing from this",
+      author: "Missing McMisserson",
+      likes: 21
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
-test('blog posts have unique identifier property "id"', async () => {
-  const response = await api.get('/api/blogs')
 
-  response.body.forEach(blog => expect(blog.id).toBeDefined())
-})
-
-test('POST request creates a new blog post', async () => {
-  const newBlog = {
-    title: 'this is a newly added blog post',
-    author: "Testy McTesterson",
-    url: "added-by-test",
-    likes: 6
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-  const contents = blogsAtEnd.map(n => n.title)
-  expect(contents).toContain(
-    'this is a newly added blog post'
-  )
-})
-
-test('if likes missing, defaults to zero', async () => {
-  const newBlog = {
-    title: 'this blog post is missing likes',
-    author: "Likey McLikerson",
-    url: "added-by-test-again"
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-  const blogToView = blogsAtEnd[blogsAtEnd.length - 1]
-
-  expect(blogToView.likes).toEqual(0)
-})
-
-test('blogs without title is not added', async () => {
-  const newBlog = {
-    author: "Missing McMisserson",
-    url: "added-by-test-three",
-    likes: 21
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-})
-
-test('blogs without url is not added', async () => {
-  const newBlog = {
-    title: "URL is missing from this",
-    author: "Missing McMisserson",
-    likes: 21
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
-})
 
 afterAll(() => {
   mongoose.connection.close()
