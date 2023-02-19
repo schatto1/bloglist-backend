7eee5848735cceb406676434b0cd6aecda8f9cae
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-02-19 00:14:15 -0800

    exercise 4.15 - feat: adds new users controller for API requests
    adds a new HTTP POST request for creating new users
    adds the call for the users controller in apps

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 require('express-async-errors')
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -24,6 +25,7 @@ app.use(express.static('build'))
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.errorHandler)
 
