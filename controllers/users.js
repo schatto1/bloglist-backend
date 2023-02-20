a2150a709ffa4d5208da06cadce585c0807d9a10
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-02-19 22:35:41 -0800

    exercise 4.16 - refactor: password is now required and must be at least 3 chars long
    No Mongoose validations used, password is validated in the users controller

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,13 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
+  if (password === undefined) {
+    return response.status(400).json({ error: 'password missing' })
+  }
+  if (password.length < 3) {
+    return response.status(400).json({error: 'password must be at least 3 characters'})
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
 
