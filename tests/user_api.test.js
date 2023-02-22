42110f9b2fb749d4bdc3e5fb18556a3ea4c37493
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-02-21 21:56:03 -0800

    exercise 4.16 - test: adds test for username requirement

--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -15,6 +15,8 @@ beforeEach(async () => {
 
 describe('addition of a new user', () => {
   test('POST request creates a new user', async () => {
+    const usersAtStart = await helper.usersInDb()
+
     const newUser = {
       username: "greeter",
       name: 'Hello McGreeterson',
@@ -28,13 +30,40 @@ describe('addition of a new user', () => {
       .expect('Content-Type', /application\/json/)
   
     const usersAtEnd = await helper.usersInDb()
-    expect(usersAtEnd).toHaveLength(helper.initialUsers.length + 1)
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
   
-    const usernames = usersAtEnd.map(n => n.username)
+    const usernames = usersAtEnd.map(u => u.username)
     expect(usernames).toContain(
       'greeter'
     )
   })
+
+  // TODO: add tests to test username requirement validation for user operations
+  test('user creation fails if username not provided', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })
+
+  // TODO: add tests to test username length validation for user operations
+  
+  // TODO: add tests to test username uniqueness validation for user operations
+  
+  // TODO: add tests to test password requirement validation for user operations
+  
+  // TODO: add tests to test username length validation for user operations
 })
 
 afterAll(() => {
