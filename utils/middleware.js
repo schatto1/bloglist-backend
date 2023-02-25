1c544048439f3854b196c4d767f75a82dfce25e5
Sumon Chattopadhyay <sumon.chattopadhyay@utah.edu>
2023-02-24 17:02:31 -0800

    exercise 4.19 - refactor: errorHandler now handles JsonWebTokenError

--- utils/middleware.js
+++ utils/middleware.js
@@ -5,6 +5,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name ===  'JsonWebTokenError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
